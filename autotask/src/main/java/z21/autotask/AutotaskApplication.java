a0edfaa8d66cc0b300a60543718278c13791c882
jkierucz <01169183@pw.edu.pl>
2022-12-05 16:11:23 +0100

    Added SpringBoot base

---
+++ autotask/src/main/java/z21/autotask/AutotaskApplication.java
@@ -0,0 +1,15 @@
+package z21.autotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AutotaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotaskApplication.class, args);
+	}
+
+}
