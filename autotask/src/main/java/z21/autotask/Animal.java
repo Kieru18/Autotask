f787879e43e80b7d189099a9faab085eb8bfe600
jkierucz <01169183@pw.edu.pl>
2022-12-06 19:00:24 +0100

    Added Lombok support.

--- autotask/src/main/java/z21/autotask/Animal.java
+++ autotask/src/main/java/z21/autotask/Animal.java
@@ -5,60 +5,29 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "animals")
 public class Animal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer animal_id;
+    @NonNull private Integer animal_id;
     private String name;
     private String color;
     private Integer leg_count;
-
-    protected Animal() {};
-
-    public Animal(int animalId, String name, String color, int legCount) {
-        this.animal_id = animalId;
-        this.name = name;
-        this.color = color;
-        this.leg_count = legCount;
-    }
 
     @Override
     public String toString() {
         return String.format(
             "Animal[animalId=%d, name='%s', color='%s', legCount=%d]",
             animal_id, name, color, leg_count);
-    }
-
-    public int getAnimalId() {
-        return this.animal_id;
-    }
-    public String getName() {
-        return this.name;
-    }
-    public String getColor() {
-        return this.color;
-    }
-    public int getLegCount() {
-        return this.leg_count;
-    }
-
-    public void setAnimalId(int animalId) {
-        this.animal_id = animalId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setLegCount(int legCount) {
-        this.leg_count = legCount;
     }
 }
