b8b9888ca522b617113b3ca22775ec49bb11a386
slukawsk <01169205@pw.edu.pl>
2022-12-27 19:59:38 +0100

    Refactored rest of components in TaskFormView

--- autotask/src/main/java/z21/autotask/views/list/TaskFormView.java
+++ autotask/src/main/java/z21/autotask/views/list/TaskFormView.java
@@ -30,38 +30,12 @@ public class TaskFormView extends VerticalLayout {
         FormLayout taskForm = new FormLayout();
 
         ComboBox<String> CBwho = prepareWhoComboBox();
+        ComboBox<String> CBtaskGroup = prepareTaskGroupComboBox();
+        ComboBox<String> CBwhere = prepareWhereComboBox();
+        DateTimePicker DTPwhen = prepareWhenDateTimePicker();
+        TextArea TADescription = prepareDescriptionTextArea();
+        HorizontalLayout buttons = prepareButtons();
 
-
-
-        ComboBox<String> CBtaskGroup = new ComboBox<>("What to do");    // TD change ComboBox<String> to ComboBox<TaskGroup>
-        ComboBox<String> CBwhere = new ComboBox<>("Where");             // TD change ComboBox<String> to ComboBox<Location>
-
-        DateTimePicker DTPwhen = new DateTimePicker();
-        DTPwhen.setLabel("When");
-        DTPwhen.setStep(Duration.ofMinutes(30));
-        DTPwhen.setValue(LocalDateTime.now());
-
-        int charLimit = 1000;
-        
-        TextArea TADescription = new TextArea("Description");
-        TADescription.setWidthFull();
-        TADescription.setMaxLength(charLimit);
-        TADescription.setValueChangeMode(ValueChangeMode.EAGER);
-        TADescription.addValueChangeListener(e -> e.getSource().setHelperText(e.getValue().length() + "/" + charLimit));
-
-        Button BSubmit = new Button("Submit");
-        Button BClear = new Button("Clear");
-        HorizontalLayout buttons = new HorizontalLayout();
-        buttons.add(BSubmit, BClear);
-
-        BSubmit.addClickListener(click -> {
-            // TD collect all data from form components, validate each input and if correct make Task class object and send to database
-            Notification.show("Not implemented: Submit");
-        });
-        BClear.addClickListener(click -> {
-            // TD clear TADescription, CBwho, CBtaskGroup, CBwhere. DTPwhen set to default
-            Notification.show("Not implemented: Clear");
-        });
         taskForm.add(CBwho, CBtaskGroup, CBwhere, TADescription, DTPwhen, buttons);
 
         H1 title = new H1("Task Generator");
@@ -80,5 +54,64 @@ public class TaskFormView extends VerticalLayout {
             CBwho.setValue(customValue);});
         return CBwho;
     }
+    private ComboBox<String> prepareTaskGroupComboBox(){
+        ArrayList<String> taskGroups = new ArrayList<String>();
+        taskGroups.add("Karmienie");
+        ComboBox<String> CBtaskGroup = new ComboBox<>("What to do");                 // TD change ComboBox<String> to ComboBox<TaskGroup>
+        CBtaskGroup.setAllowCustomValue(true);
+        CBtaskGroup.addCustomValueSetListener(e -> {
+            String customValue = e.getDetail();
+            taskGroups.add(customValue);
+            CBtaskGroup.setItems(taskGroups);
+            CBtaskGroup.setValue(customValue);});
+        return CBtaskGroup;
+    }
+    private ComboBox<String> prepareWhereComboBox(){
+        ArrayList<String> locations = new ArrayList<String>();
+        locations.add("Wybieg dla Słonia");
+        ComboBox<String> CBwhere = new ComboBox<>("Where");                 // TD change ComboBox<String> to ComboBox<Locations>
+        CBwhere.setAllowCustomValue(true);
+        CBwhere.addCustomValueSetListener(e -> {
+            String customValue = e.getDetail();
+            locations.add(customValue);
+            CBwhere.setItems(locations);
+            CBwhere.setValue(customValue);});
+        return CBwhere;
+    }
+    private DateTimePicker prepareWhenDateTimePicker()
+    {
+        DateTimePicker DTPwhen = new DateTimePicker();
+        DTPwhen.setLabel("When");
+        DTPwhen.setStep(Duration.ofMinutes(30));
+        DTPwhen.setValue(LocalDateTime.now());
+        return DTPwhen;
+    }
+    private TextArea prepareDescriptionTextArea()
+    {
+        int charLimit = 1000;
+        TextArea TADescription = new TextArea("Description");
+        TADescription.setWidthFull();
+        TADescription.setMaxLength(charLimit);
+        TADescription.setValueChangeMode(ValueChangeMode.EAGER);
+        TADescription.addValueChangeListener(e -> e.getSource().setHelperText(e.getValue().length() + "/" + charLimit));
+        return TADescription;
+    }
 
+    private HorizontalLayout prepareButtons()
+    {
+        Button BSubmit = new Button("Submit");
+        Button BClear = new Button("Clear");
+        HorizontalLayout buttons = new HorizontalLayout();
+        buttons.add(BSubmit, BClear);
+
+        BSubmit.addClickListener(click -> {
+            // TD collect all data from form components, validate each input and if correct make Task class object and send to database
+            Notification.show("Not implemented: Submit");
+        });
+        BClear.addClickListener(click -> {
+            // TD clear TADescription, CBwho, CBtaskGroup, CBwhere. DTPwhen set to default
+            Notification.show("Not implemented: Clear");
+        });
+        return buttons;
+    }
 }
