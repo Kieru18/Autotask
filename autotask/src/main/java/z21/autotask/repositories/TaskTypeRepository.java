dd4c8e38c6f5d7c8b5d4ad09223eea1ac5b93490
kziarek <01169264@pw.edu.pl>
2022-12-31 13:02:24 +0100

    Created basic task-related repositories
    Added TaskRepository, TaskTypeRepository, TaskStatusRepository with basic functionalities

---
+++ autotask/src/main/java/z21/autotask/repositories/TaskTypeRepository.java
@@ -0,0 +1,38 @@
+package z21.autotask.repositories;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import z21.autotask.orm.TaskType;
+
+
+@Transactional @Repository
+public interface TaskTypeRepository extends JpaRepository<TaskType, Integer> {
+    @Query
+    List<TaskType> findByTypeId(Integer typeId);
+
+    @Query
+    List<TaskType> findByName(String name);
+
+    @Query
+    List<TaskType> findByDescription(String description);
+
+    @Query
+    List<TaskType> findByBasePriority(Integer basePriority);
+
+    @Query
+    List<TaskType> findByFrequency(SimpleDateFormat frequency);
+
+
+    @Modifying @Query(value = "INSERT INTO task_types (name,  description, base_priority, frequency) VALUES (:name, :description, :basePriority, :frequency )", nativeQuery = true)
+    void insertTaskType(@Param("name") String name, @Param("description")  String description, @Param("base_priority") Integer basePriority,
+                        @Param("frequency") SimpleDateFormat frequency);
+
+}
