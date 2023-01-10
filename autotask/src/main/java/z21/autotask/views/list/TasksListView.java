ce1ec10b9c096fac01c52e4bd9abd447714c8927
kziarek <01169264@pw.edu.pl>
2023-01-10 14:29:07 +0100

    Added buttons to display lists of assigned employees/animals

--- autotask/src/main/java/z21/autotask/views/list/TasksListView.java
+++ autotask/src/main/java/z21/autotask/views/list/TasksListView.java
@@ -1,14 +1,20 @@
 package z21.autotask.views.list;
 
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.virtuallist.VirtualList;
+
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.dom.ElementFactory;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -21,6 +27,7 @@ import z21.autotask.entities.Task;
 import z21.autotask.service.DataService;
 import z21.autotask.views.MainLayout;
 
+import java.time.ZoneId;
 import java.util.List;
 
 @PageTitle("List of all tasks")
@@ -30,12 +37,15 @@ public class TasksListView extends VerticalLayout {
     Grid<Task> grid = new Grid<>(Task.class, false);
     TextField filterText = new TextField();
 
+    Dialog dialogEmployees = new Dialog();
+    Dialog dialogAnimals = new Dialog();
 
     @Autowired
     public TasksListView(DataService dataService) {
         this.dataService = dataService;
         addClassName("list-view");
         setSizeFull();
+        configureDialogs();
         configureGrid();
 
         add(getToolbar(), grid);
@@ -56,16 +66,17 @@ public class TasksListView extends VerticalLayout {
         grid.addColumn(Task -> Task.getLocation().getName()).setHeader("Location");
         grid.addColumn(Task -> Task.getType().getName()).setHeader("Type");
 
-        grid.addColumn(new ComponentRenderer<>(Task -> {
-            VirtualList<Employee> list = new VirtualList<>();
-            list.setItems(dataService.getEmployeesByTask(Task));
-            return list;
+        grid.addColumn(new ComponentRenderer<>(task -> {
+            Button buttonEmp = new Button("Show employees", e -> dialogEmployees.open());
+            updateEmployeeDialog(task);
+            return buttonEmp;
+
             })).setHeader("Employees");
 
-        grid.addColumn(new ComponentRenderer<>(Task -> {
-            VirtualList<Animal> list = new VirtualList<>();
-            list.setItems(dataService.getAnimalsByTask(Task));
-            return list;
+        grid.addColumn(new ComponentRenderer<>(task -> {
+            Button buttonAnm = new Button("Show animals", e -> dialogAnimals.open());
+            updateAnimalDialog(task);
+            return buttonAnm;
         })).setHeader("Animals");
 
         List<Task> listOfTasks = dataService.getAllTasks();
@@ -91,5 +102,93 @@ public class TasksListView extends VerticalLayout {
         toolbar.addClassName("toolbar");
         return toolbar;
     }
+
+    void configureDialogs(){
+        dialogEmployees.setHeaderTitle("List of assigned employees");
+        dialogAnimals.setHeaderTitle("List of assigned animals");
+
+        dialogEmployees.setMinWidth("680px");
+        dialogAnimals.setMinWidth("880px");
+
+        VerticalLayout dialogEmployeeLayout = getEmployeesCards(dataService.getAllEmployees());
+        VerticalLayout dialogAnimalLayout = getAnimalsList(dataService.getAllAnimals());
+
+        dialogEmployees.add(dialogEmployeeLayout);
+        dialogAnimals.add(dialogAnimalLayout);
+
+        dialogEmployees.getFooter().add(new Button("Close", e -> dialogEmployees.close()));
+        dialogAnimals.getFooter().add(new Button("Close", e -> dialogAnimals.close()));
+    }
+
+
+    private void updateEmployeeDialog(Task task){
+        dialogEmployees.removeAll();
+        dialogEmployees.add(getEmployeesCards(dataService.getEmployeesByTask(task)));
+
+    }
+    private void updateAnimalDialog(Task task){
+        dialogAnimals.removeAll();
+        dialogAnimals.add(getAnimalsList(dataService.getAnimalsByTask(task)));
+    }
+
+    private VerticalLayout getAnimalsList(List<Animal> animals){
+        VerticalLayout verticalLayoutAnimals = new VerticalLayout();
+        Grid<Animal> grid = new Grid<>(Animal.class, false);
+
+        grid.addColumn(Animal::getAnimalId).setHeader("ID");
+        grid.addColumn(Animal::getName).setHeader("Name");
+        grid.addColumn(Animal -> Animal.getBirthDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate()).setHeader("Date of birth");
+        grid.addColumn(Animal::getWeight).setHeader("Weight");
+        grid.addColumn(Animal -> Animal.getLocation().getName()).setHeader("Location");
+        grid.addColumn(Animal -> Animal.getSpecies().getName()).setHeader("Species");
+
+        grid.setItems(animals);
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        verticalLayoutAnimals.add(grid);
+        return verticalLayoutAnimals;
+    }
+
+    private VerticalLayout getEmployeesCards(List<Employee> employees)
+    {
+        VerticalLayout verticalLayoutEmployees = new VerticalLayout();
+        for(Employee employee : employees)
+        {
+            verticalLayoutEmployees.add(createEmployeeCardComponent(employee));
+        }
+        return verticalLayoutEmployees;
+    }
+    private HorizontalLayout createEmployeeCardComponent(Employee person)
+    {
+        HorizontalLayout cardLayout = new HorizontalLayout();
+        cardLayout.setMargin(true);
+
+        Avatar avatar = new Avatar(person.getFullName());
+        avatar.setImage(person.getPictureRoute());
+
+        avatar.setHeight("64px");
+        avatar.setWidth("64px");
+
+        VerticalLayout infoLayout = new VerticalLayout();
+        infoLayout.setSpacing(false);
+        infoLayout.setPadding(false);
+        infoLayout.getElement().appendChild(
+                ElementFactory.createStrong(person.getFullName()));
+        infoLayout.add(new Div(new Text(person.getProfession())));
+
+
+        VerticalLayout contactLayout = new VerticalLayout();
+        contactLayout.setSpacing(false);
+        contactLayout.setPadding(false);
+        if(person.getUser() != null) {
+            contactLayout.add(new Div(new Text(person.getEmail())));
+            infoLayout.add(new Details("Contact information", contactLayout));
+        }
+
+
+        cardLayout.add(avatar, infoLayout);
+        return cardLayout;
+    }
+
 }
 
